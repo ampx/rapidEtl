9709a5e1d677d6e287fc4ec182c53098c33566bd
ampx <ampx@users.noreply.github.com>
2020-06-21 09:57:02 -0700

    Update and rename fin/db/ticker_prices.sql to fin/db/findb/ticker_prices.sql

--- fin/db/findb/ticker_prices.sql
+++ fin/db/findb/ticker_prices.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `findb.index_price_daily` (
+CREATE TABLE `index_price_daily` (
   `Date` date DEFAULT NULL,
+  `ticker` varchar(20) DEFAULT NULL,
   `open` float DEFAULT NULL,
   `high` float DEFAULT NULL,
   `low` float DEFAULT NULL,
   `close` float DEFAULT NULL,
   `adj_close` float DEFAULT NULL,
   `volume` float DEFAULT NULL,
-  UNIQUE KEY(Date) 
-) ENGINE=InnoDB)
+  UNIQUE KEY(Date, ticker) 
+) ENGINE=InnoDB
